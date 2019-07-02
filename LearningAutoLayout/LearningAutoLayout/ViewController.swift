1f9991e7ac539e17f01cffd572aa411eeed4a53d
Kunal Barde <kunalbarde@Kunals-MacBook-Pro.local>
2019-07-02 16:47:14 -0400

    Learning Auto Layout to solve Layout Problem

---
+++ LearningAutoLayout/LearningAutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearningAutoLayout
+//
+//  Created by Kunal Barde on 7/2/19.
+//  Copyright © 2019 talent@berg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
