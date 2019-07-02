6c5469c0c93912fdc735122e4dd7654a902f371f
Kunal Barde <kunalbarde@Kunals-MacBook-Pro.local>
2019-07-02 16:44:22 -0400

    Working with Boolean Data Type - Unary, Binary and Ternary Operations

---
+++ Basics - Swift/Booleans.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var isLightOn = true
+
+// Unary Operator
+//isLightOn = !isLightOn
+
+// Binary Operator
+var isBatmanAmazing = true
+var name = "Kunal"
+var year = 2019
+
+// Ternary Operator
+var isItDark = false
+isItDark = isLightOn ? false : true
